f3454713a06f76c21dc7293e8db51e6ca5fb9054
carriegnp <carriegnp@gmail.com>
2019-08-17 13:25:09 -0500

    Initial commit

---
+++ TextbookApplication.java
@@ -0,0 +1,15 @@
+package com.xiao.textbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class TextbookApplication {
+
+	public static void main(String[] args) {
+		run(TextbookApplication.class, args);
+	}
+
+}
